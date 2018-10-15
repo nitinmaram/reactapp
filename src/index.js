d1d37d13fe5ebdae3e0cc2cb7d4e87fce3ecfab1
nitinmaram <nitinmaram@gmail.com>
2018-10-15 21:39:28 +0530

    added filter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {HashRouter, Route} from 'react-router-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import TableApp from './tableApp';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <MuiThemeProvider>
+  <HashRouter>
+      <div >
+      <Route exact path='/' component={App} />
+      <Route exact path='/table' component={TableApp} />
+      </div>
+  </HashRouter>
+  </MuiThemeProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/tableApp.js
@@ -0,0 +1,154 @@
+import React, { Component } from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+class TableApp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      startDate: '',
+      endDate: '',
+      fixData: []
+    }
+  }
+  async componentDidMount() {
+    try  {
+      const response = await fetch(`http://localhost:5000`)
+      const json = await response.json()
+      this.setState({ data: json.data, fixData: json.data });
+      console.log(this.state.data);
+  }
+    catch(err) {
+      const response = await fetch(`http://localhost:5000`)
+      const text = await response.text()
+    }
+  }
+  headerClick(e){
+    this.setState({
+      [e]: true
+    })
+    if(this.state[e])
+  {
+    if(e == 'name'){
+      this.state.data.sort(function (a,b) {
+        if(a.name.toUpperCase() < b.name.toUpperCase())
+        {
+          return -1
+        }
+        if(a.name.toUpperCase() > b.name.toUpperCase()){
+          return 1
+        }
+        return 0
+      })
+    }
+    if(e=='amount')
+    {
+      this.state.data.sort(function(a,b){
+      return b[e]-a[e]
+    })
+  }
+  if(e=='date'){
+    this.state.data.sort(function(a,b) {
+      a = new Date(a[e]);
+      b = new Date(b[e]);
+       return a-b;
+    })
+  }
+    this.setState({
+      [e]: false
+    })
+  }
+  else{
+    if(e=='name'){
+      this.state.data.sort(function (a,b) {
+        if(b.name.toUpperCase() < a.name.toUpperCase()){
+          return -1
+        }
+        if(b.name.toUpperCase() > a.name.toUpperCase()){
+          return 1
+        }
+        return 0
+      })
+    }
+    if(e=='amount')
+    {
+      this.state.data.sort(function(a,b){
+      return a[e]-b[e]
+    })
+  }
+  if(e=='date'){
+    this.state.data.sort(function(a,b) {
+      a = new Date(a[e]);
+      b = new Date(b[e]);
+      return b-a
+    })
+  }
+    this.setState({
+      [e]: true
+    })
+  }
+    this.setState({
+      data: this.state.data
+    })
+  }
+  handleSubmit(){
+    var sd = new Date(document.getElementById('sd').value)
+    var ed = new Date(document.getElementById('ed').value)
+    var res = this.state.fixData.filter(function (obj) {
+      console.log(new Date(obj.date) <= ed);
+      if(new Date(obj.date) >= sd && new Date(obj.date) <= ed){
+        return true
+      }
+      else{
+        return false
+      }
+    })
+    this.setState({
+      data: res
+    })
+  }
+render(){
+
+  return (
+    <div>
+    <h1 style = {{marginLeft: '5%'}}>Transactions</h1>
+
+    <Paper style = {{width: '30%',marginLeft: '5%'}}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell onClick = {this.headerClick.bind(this,'name')}>Name</TableCell>
+            <TableCell onClick = {this.headerClick.bind(this,'amount')}>Amount</TableCell>
+            <TableCell onClick = {this.headerClick.bind(this,'date')}>Date</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {this.state.data.map(row => {
+            return (
+              <TableRow key={row.name}>
+                <TableCell>
+                  {row.name}
+                </TableCell>
+                <TableCell>{row.amount}</TableCell>
+                <TableCell>{new Date(row.date).toDateString()}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
+    <div style = {{marginLeft: '5%', marginTop: '2%'}}>
+    <input type = 'text' id = 'sd' placeHolder = "mm-dd-yyyy" /><br/>
+    <input type = 'text' id = 'ed' placeHolder = "mm-dd-yyyy" /><br/>
+    <button onClick = {this.handleSubmit.bind(this)}>submit</button>
+    </div>
+    </div>
+  );
+}
+}
+export default TableApp;
